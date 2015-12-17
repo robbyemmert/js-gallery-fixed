33ced15bb01c57d99d42ef2f3e1147b1da1bcb09
Robby Emmert <robby@robbyemmert.com>
2015-12-16 22:02:21 -0500

    backup

--- src/js-gallery-fixed.js
+++ src/js-gallery-fixed.js
@@ -2,8 +2,50 @@ var $ = require('./modules/jqueryLoader.js');
 
 class FixedGallery {
     constructor(element){
+        this.queuedImages = [];
+        this.imageBlocks = [];
+
         if (!(element instanceof $)) {
             element = $(element);
         }
+
+        element.wrapInner('<div class="image-wrapper aspect"></div>');
+        this.wrapper = element.find('.image-wrapper');
+
+        element.find('gallery-image').each((i, img) => {
+            let galleryImage = new GalleryImage(img);
+            galleryImage.element.detach();
+            this.queuedImages.push(galleryImage);
+            this.insertImage(galleryImage);
+        });
+
+        this.element = element;
+    }
+
+    insertImage(img) {
+        let blocks = this.wrapper.find('.image-block');
+        if (blocks.length <= 0) {
+            this.addBlock(this.wrapper, img);
+        }
+    }
+
+    addBlock(context, img) {
+        let type = (context[0].offsetWidth > context[0].offsetHeight) ? 'col' : 'row';
+        let block = $(`<div class="image-block"></div>`);
+        block.append(img.element);
+        context.append(block);
     }
 }
+
+class GalleryImage {
+    constructor(element){
+        if (!(element instanceof $)) {
+            element = $(element);
+        }
+
+        element.css('background-image', `url("${ element.attr('src') }")`);
+        this.element = element;
+    }
+}
+
+window.FixedGallery = FixedGallery;
--- src/modules/jqueryLoader.js
+++ src/modules/jqueryLoader.js
@@ -2,4 +2,11 @@ var $ = window.$ || window.jQuery;
 if (!$) {
     throw new Error('module js-gallery-fixed requires jQuery on the global scope');
 }
+
+$(document).ready(() => {
+    $('gallery').each((i, elm) => {
+        let gallery = new FixedGallery(elm);
+    });
+});
+
 module.exports = $;
